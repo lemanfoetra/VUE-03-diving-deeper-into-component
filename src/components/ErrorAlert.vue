9fbe77b974c90d35a3a18bc583e77732c493e84f
Ardyn Sulaeman <lemanfoetra@gmail.com>
2023-10-25 16:53:46 +0700

    Applying What We Know & A Problem : penerapan tag <dialog> untuk menampilkan pesan error

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,21 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  right: 40%;
+  background-color: white;
+  padding: 10px;
+}
+</style>
+
+<script>
+export default {
+};
+</script>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,9 +1,37 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text">
+  <input type="text" ref="goal">
+  <button type="button" @click="setGoal">Set Goal</button>
+
+  <ErrorAlert v-if="inputIsInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Mohon masukan sebuah data</p>
+    <button type="button" @click="confirmError">Oke</button>
+  </ErrorAlert>
 </template>
 
 <script>
+import ErrorAlert from './ErrorAlert.vue'
+
 export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
+  methods: {
+    setGoal() {
+      const eneterdValue = this.$refs.goal.value;
+      if (eneterdValue === '') {
+        this.inputIsInvalid = true
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false
+    }
+  }
 };
 </script>
