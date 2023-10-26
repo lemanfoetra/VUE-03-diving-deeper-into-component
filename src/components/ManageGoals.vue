05ca9de0a60d4d4dbe2a13cc988a31ae98389a69
Ardyn Sulaeman <lemanfoetra@gmail.com>
2023-10-26 15:18:07 +0700

    Teleport - Tag Teleport

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -2,12 +2,13 @@
   <h2>Manage Goals</h2>
   <input type="text" ref="goal">
   <button type="button" @click="setGoal">Set Goal</button>
-
-  <ErrorAlert v-if="inputIsInvalid">
-    <h2>Input is invalid!</h2>
-    <p>Mohon masukan sebuah data</p>
-    <button type="button" @click="confirmError">Oke</button>
-  </ErrorAlert>
+  <Teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Mohon masukan sebuah data</p>
+      <button type="button" @click="confirmError">Oke</button>
+    </ErrorAlert>
+  </Teleport>
 </template>
 
 <script>
